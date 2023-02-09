0ae4ac187edb46049e01306a36d02ea5cc5a4d95
AngelikaGralewska <105511457+AngelikaGralewska@users.noreply.github.com>
2023-02-09 22:18:55 +0100

    Update contactsSlice.js

--- src/redux/contactsSlice.js
+++ src/redux/contactsSlice.js
@@ -18,7 +18,7 @@ const contactsSlice = createSlice({
 
   reducers: {
     addContact(state, action) {
-      state.contacts = action.payload;
+      state.contacts.push(action.payload)
 },
     deleteContact(state, action) { 
         state.contacts = state.contacts.filter(contact => contact.id !== action.payload);      
